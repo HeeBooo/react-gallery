f16b737d58ac4765a2cff2e23464dee02b79b71a
HeeBooo <1807941596@qq.com>
2017-10-07 17:00:28 +0800

    deleted imgFigure.js file

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 // 图片相关信息
 import imageDatas from '../data/imageDatas.json';
 
@@ -14,10 +15,46 @@ import imageDatas from '../data/imageDatas.json';
 
 
 import '../scss/index.scss';
-// 单个图片
-import ImgFigure from './imgFigure';
 
-class Gallery extends React.Component {
+
+// 单个图片
+function ImgFigure (props) {
+    return (
+        <figure className="img-figure">
+            <img src={props.data.imageURL}
+                 alt={props.data.title} />
+            <figcaption>
+                <h2 className="img-title">{props.data.desc}</h2>
+            </figcaption>
+        </figure>
+    )
+}
+
+
+class Gallery extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            constant: {
+                centerPos: {
+                    left: 0,
+                    right: 0
+                },
+                // 水平方向取值范围
+                hPosRange: {
+                    leftSecX: [0, 0],
+                    rightSecX: [0, 0],
+                    y: [0, 0]
+                },
+                // 垂直方向取值范围
+                vPosRange: {
+                    x: [0, 0],
+                    topy: [0, 0]
+                }
+            }
+        }
+    };
+    
     componentDidMount () {
         // 将图片信息转为图片URL路径信息
         let data = (function genImageURL (imageDatasArr) {
@@ -29,7 +66,22 @@ class Gallery extends React.Component {
             }
             return imageDatasArr;
         })(imageDatas);
+        // 组件加载以后，为每张图片计算其位置的范围
+        // 获取stage的节点
+        let stageDom = ReactDOM.findDOMNode(this.refs.stage);
+        // stageW = stageDom.scrollWidth,
+        // stageH = stageDom.scrollHeight;
+        console.log(stageDom)
+
+        
+        
+
         return data;
+        
+        
+    };
+    componentDidUpdate () {
+
     }
     render () {
         let controllerUnits = [],
@@ -40,9 +92,9 @@ class Gallery extends React.Component {
         data.forEach((value, index) => {
             imgFigures.push(<ImgFigure data={value} key={'imgFigures'+index} />)
         })
-
+        
         return (
-            <section className="stage">
+            <section className="stage" ref="stage">
                 <section className="img-sec">
                     {imgFigures}
                 </section>
@@ -53,5 +105,10 @@ class Gallery extends React.Component {
         )
     }
 }
-
+// 左分区 0 - ImgFigure.width/2 < x < stage.width/2 - ImgFigure.width/2*3
+//       0 - ImgFigure.height/2 < y < stage.height - ImgFigure.height/2
+// 右分区 stage.width/2 + ImgFigure.width/2 < x < stage.width - ImgFigure.width/2
+//       0 - ImgFigure.height/2 < y < stage.height - ImgFigure.height/2
+// 上分区 stage.width/2 - ImgFigure.width < x < stage.width/2
+//       0 - ImgFigure.height/2 < y < stage.height - ImgFigure.height/2*3
 export default Gallery;
